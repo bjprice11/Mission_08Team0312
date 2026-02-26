8f033f769bec2e1de4fb10119138bf93c1145f90
elifober <eliasfobert@gmail.com>
2026-02-25 18:08:21 -0700

    Foreign key for category Create the add update and delete routes to context
    In order to change the database I need the project to be able to compile. Once all other errors are fixed I can update the database

--- Mission_08/Models/ITasksRepository.cs
+++ Mission_08/Models/ITasksRepository.cs
@@ -3,4 +3,11 @@ namespace Mission_08.Models;
 public interface ITasksRepository
 {
     List<TaskItem> TaskItems { get; }
+    List<Category> Categories { get; }
+
+    public void AddTask(TaskItem taskItem);
+    
+    public void UpdateTask(TaskItem taskItem);
+    
+    public void DeleteTask(TaskItem taskItem);
 }
